a2add21127a6d2685c24eec6f26c824c2515ac6d
Soojung Kwak <gsj2464@gmail.com>
2020-03-06 15:15:07 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Game from './components/Game';
+import './App.css';
+
+
+const StarMatch = () => {
+  const [gameId, setGameId] = useState(1);
+  return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />;
+};
+
+function App() {
+  return (
+    <StarMatch />
+  );
+}
+
+export default App;
